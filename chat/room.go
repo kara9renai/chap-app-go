28aa6c5512c582725a1a60a12c07dc7ab3488e7d
kara9renai <80733629+kara9renai@users.noreply.github.com>
2022-02-15 21:23:34 +0900

    add ServeHTTP in room.go

--- chat/room.go
+++ chat/room.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/kara9renai/chat-app-go/trace"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"github.com/kara9renai/chat-app-go/trace"
+)
 
 type room struct {
 	forward chan []byte
@@ -21,10 +27,45 @@ func newRoom() *room {
 }
 
 func (r *room) run() {
-
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+			r.tracer.Trace("New client joined")
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.send)
+			r.tracer.Trace("client left")
+		case msg := <-r.forward:
+			r.tracer.Trace("Message received: ", string(msg))
+			for client := range r.clients {
+				client.send <- msg
+				r.tracer.Trace(" -- sent to client")
+			}
+		}
+	}
 }
 
 const (
 	socketBufferSize  = 1024
 	messageBufferSize = 256
 )
+
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBufferSize: messageBufferSize}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP: ", err)
+		return
+	}
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+	r.join <- client
+	defer func() { r.leave <- client }()
+	go client.write()
+	client.read()
+}
